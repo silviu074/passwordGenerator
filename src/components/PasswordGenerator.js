2bf3ca22b9fff3f6a1c0358108f635a523c038c3
Silvio <silviu074@gmail.com>
2022-09-14 22:26:16 +0300

    synchronizing input values with useState

--- src/components/PasswordGenerator.js
+++ src/components/PasswordGenerator.js
@@ -14,11 +14,30 @@ import React, { useState } from 'react'
 
 export const PasswordGenerator =() =>  {
 
-    // const [characterAmountNumber, setcharacterAmountNumber] = useState(6)
-    // const [characterAmountRange, setcharacterAmountRange] = useState(6)
+    const [characterAmountNumber, setcharacterAmountNumber] = useState(6)
+    const [characterAmountRange, setcharacterAmountRange] = useState(6)
+    const [hasUpperCaseLetters, setHasUpperCaseLetters] = useState(false)
+    const [hasNumbers, setHasNumbers] = useState(false)
+    const [hasSymbols, setHasSymbols] = useState(false)
+
+    const emptyArray=[]
+    const UPPERCASE = arrayAnsi(97.122)
+    const LOWERCASE = arrayAnsi(65.90)
+    const NUMBERS = arrayAnsi(48.57)
+    const SYMBOLS = arrayAnsi(33.47).concat(arrayAnsi(58,64)).concat(arrayAnsi(91,96)).concat(arrayAnsi(123,126))
+    console.log(UPPERCASE)
+    console.log(LOWERCASE)
+    console.log(NUMBERS)
+    console.log(SYMBOLS)
 
 
-
+    function arrayAnsi(min,max){
+      const array=[]
+      for(let i=min;i<=max;i++){
+        array.push(i)
+      }
+      return array
+    }
 
    return (
     <div>
@@ -26,23 +45,60 @@ export const PasswordGenerator =() =>  {
         <h1>Password Generator</h1>
         <div className="inputs">
 
-            <div>
+        <div>
          <label htmlFor='minumumLengthNumber' >Minimum lenth: </label>
-         <input type="range" id="minumumLengthRange" min={6} max={25} value={6}/>
-         <input type="number" id="minumumLengthNumber"  min={6} max={25}  value={6}/>
+
+         <input type="range"
+            id="minumumLengthRange"
+            min={6}
+            max={25} 
+            value={characterAmountNumber} 
+            onChange={ (e) => {
+              const value=e.target.value;
+              setcharacterAmountNumber(value);
+              setcharacterAmountRange(value);
+           } }/>
+
+         <input type="number" 
+            id="minumumLengthNumber" 
+            min={6} 
+            max={25}  
+            value={characterAmountRange}
+            onChange={ (e) => {
+              const value=e.target.value;
+              setcharacterAmountNumber(value);
+              setcharacterAmountRange(value);
+           } }/>
          </div>
 
          <label htmlFor='hasUpperCaseLetters' >At least one uppercase letter: </label>
-         <input type="checkbox" id="hasUpperCaseLetters"  />
+         <input 
+          type="checkbox" 
+          id="hasUpperCaseLetters"
+          value={hasUpperCaseLetters}
+          onChange={ () => {
+            setHasUpperCaseLetters(current => !current)
+         } }
+          />
 
          <label htmlFor='hasNumbers' >At least one number: </label>
-         <input type="checkbox" id="hasNumbers"  />
+         <input 
+           type="checkbox" 
+           id="hasNumbers"
+           value={hasNumbers}
+           onChange={ () => {
+            setHasNumbers(current => !current)
+         } }  />
 
          <label htmlFor='hasSymbols' >At least one symbol: </label>
-         <input type="checkbox" id="hasSymbols"  />
-  
+         <input 
+          type="checkbox" 
+          id="hasSymbols"
+          value={hasSymbols}
+          onChange={ () => {
+            setHasSymbols(current => !current)
+         } }  />
         </div>
-        
         <button type='submit'>Generate Password</button>
 
       </div>
